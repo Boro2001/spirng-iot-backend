dd200e1908a7678f3715135ac64e7c9626bf10f2
bartosz <bartoszkruczek@gmail.com>
2023-01-10 22:34:41 +0100

    credentiale co dziala na aws

--- src/main/java/com/example/spirngiotbackend/MqttPubSubService.java
+++ src/main/java/com/example/spirngiotbackend/MqttPubSubService.java
@@ -20,8 +20,8 @@ public class MqttPubSubService {
     MqttPubSubService() throws AWSIotException {
         String clientEndpoint = "ag8bpew4rvau3-ats.iot.eu-west-1.amazonaws.com";   // use value returned by describe-endpoint --endpoint-type "iot:Data-ATS"
         String clientId = "spring-app";                              // replace with your own client ID. Use unique client IDs for concurrent connections.
-        String certificateFile = "/Users/bartosz/Downloads/iot_dziala_aws/nowecertyfikaty/2d801a703b265c3c2b40e6eaec6261b8b9acd1bf949eaad76ec741fd38d886c2-certificate.pem.crt";                       // X.509 based certificate file
-        String privateKeyFile = "/Users/bartosz/Downloads/iot_dziala_aws/nowecertyfikaty/2d801a703b265c3c2b40e6eaec6261b8b9acd1bf949eaad76ec741fd38d886c2-private.pem.key";
+        String certificateFile = "/home/ec2-user/credentials2/2d801a703b265c3c2b40e6eaec6261b8b9acd1bf949eaad76ec741fd38d886c2-certificate.pem.crt";                       // X.509 based certificate file
+        String privateKeyFile = "/home/ec2-user/credentials2/2d801a703b265c3c2b40e6eaec6261b8b9acd1bf949eaad76ec741fd38d886c2-private.pem.key";
         KeyStorePasswordPair pair = SampleUtil.getKeyStorePasswordPair(certificateFile, privateKeyFile);
         AWSIotMqttClient client = new AWSIotMqttClient(clientEndpoint, clientId, pair.keyStore, pair.keyPassword);
         client.connect();
