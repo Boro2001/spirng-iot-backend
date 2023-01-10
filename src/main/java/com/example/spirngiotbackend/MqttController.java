5db91c8df1387e6e366dd56a3df971ffe393efb3
plgborowiczm <mikolajborowicz7@gmail.com>
2023-01-10 13:10:57 +0100

    o

--- src/main/java/com/example/spirngiotbackend/MqttController.java
+++ src/main/java/com/example/spirngiotbackend/MqttController.java
@@ -1,2 +1,18 @@
-package com.example.spirngiotbackend;public class MqttController {
+package com.example.spirngiotbackend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MqttController {
+
+    @Autowired
+    MqttPubSubService service;
+    @PostMapping("/publish")
+    public String publishMessage() {
+
+
+        return "message published";
+    }
 }
--- src/main/java/com/example/spirngiotbackend/MqttPubSubService.java
+++ src/main/java/com/example/spirngiotbackend/MqttPubSubService.java
@@ -1,2 +1,20 @@
-package com.example.spirngiotbackend;public class MqttPubSubService {
+package com.example.spirngiotbackend;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MqttPubSubService {
+
+    MqttPubSubService() {
+        String clientEndpoint = "<prefix>-ats.iot.<region>.amazonaws.com";   // use value returned by describe-endpoint --endpoint-type "iot:Data-ATS"
+        String clientId = "<unique client id>";                              // replace with your own client ID. Use unique client IDs for concurrent connections.
+        String certificateFile = "<certificate file>";                       // X.509 based certificate file
+        String privateKeyFile = "<private key file>";
+        KeyStorePasswordPair pair = SampleUtil.getKeyStorePasswordPair(certificateFile, privateKeyFile);
+        AWSIotMqttClient client = new AWSIotMqttClient(clientEndpoint, clientId, pair.keyStore, pair.keyPassword);
+    }
+    public void publishMessage(){
+
+    }
 }
