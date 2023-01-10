82b2682d374b32ff75c0f3bf2374d334761bc2f6
plgborowiczm <mikolajborowicz7@gmail.com>
2023-01-10 13:10:31 +0100

    test

---
+++ src/main/java/com/example/spirngiotbackend/SpirngIotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirngiotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngIotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngIotBackendApplication.class, args);
+	}
+
+}
