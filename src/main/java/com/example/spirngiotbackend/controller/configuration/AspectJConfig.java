b0c68aae3152920040cccffe5ebe9bf3c7a4e241
plgborowiczm <mikolajborowicz7@gmail.com>
2023-01-14 20:45:11 +0100

    halla

--- src/main/java/com/example/spirngiotbackend/controller/configuration/AspectJConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.spirngiotbackend.controller.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
-
-@ComponentScan
-@EnableSpringConfigured
-public class AspectJConfig {
-}
