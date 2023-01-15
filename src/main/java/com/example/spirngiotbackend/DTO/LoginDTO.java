30f725ed31675ce086cd0fc27d2f1a8178f54213
plgborowiczm <mikolajborowicz7@gmail.com>
2023-01-15 16:33:42 +0100

    amen

--- src/main/java/com/example/spirngiotbackend/DTO/LoginDTO.java
+++ src/main/java/com/example/spirngiotbackend/DTO/LoginDTO.java
@@ -3,13 +3,10 @@ package com.example.spirngiotbackend.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class LoginDTO {
-    @NotBlank
     private String username;
-    @NotBlank
     private String password;
 }
